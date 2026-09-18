53bf57e02624bd665e9f6e307e5f496c95e276c2
yadav chandan <dilipyadav9956@gmail.com>
2026-09-18 17:29:44 +0530

    keep asking the password until user enter the rightpass

---
+++ password.py
@@ -0,0 +1,12 @@
+password=input("enter you password:")
+correct_password="yadav123"
+
+while password!=correct_password:
+    
+
+    if password==correct_password:
+        break
+    else:
+        password=input("enter your password again ::")
+
+print("successfully entered...")
